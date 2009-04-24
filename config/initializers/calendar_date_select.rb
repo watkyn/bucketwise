c906c0efecc5d0588dd19893a902b213a703912a
Tony Eichelberger <tony.eich@gmail.com>
2009-04-24 16:20:35 -0500

    had to change a few things to get deployment to heroku to work, still now sure what I did to get it to work :(

--- config/initializers/calendar_date_select.rb
+++ config/initializers/calendar_date_select.rb
@@ -1,2 +1,3 @@
+# TODO - was having trouble deploying to heroku becuase of these?  Now it works fine.
 CalendarDateSelect.format = :iso_date
 CalendarDateSelect.default_options[:year_range] = 10.years.ago.to_date..Date.today
